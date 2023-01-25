87a5318bafa733141d91468f3e33f96f4529c2d2
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-01-24 21:43:48 -0300

    Iniciadas aulas de JavaScript com CRUD assíncrono

---
+++ CRUD com JavaScript assíncrono/admin/service/cliente-service.js
@@ -0,0 +1,10 @@
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        resposta.json();
+    })
+}
+
+export const clienteService = {
+    listaClientes
+}
