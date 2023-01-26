26745077c3447ef792e51510646a0d5b59946250
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-01-25 21:18:28 -0300

    Curso de CRUD Assíncrono com JavaScript finalizado

--- CRUD com JavaScript assíncrono/admin/service/cliente-service.js
+++ CRUD com JavaScript assíncrono/admin/service/cliente-service.js
@@ -1,10 +1,79 @@
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
-        resposta.json();
+        if(resposta.ok){
+            resposta.json();
+        }else{
+            throw new Error('Não foi possível listar os clientes');
+        }
+    })
+}
+
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta => {
+        if(resposta.ok){
+            return resposta.body;
+        }else{
+            throw new Error('Não foi possível criar um cliente!');
+        }
+    })
+}
+
+const removeCliente = () => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    }).then(resposta => {
+        if(!resposta.ok){
+            throw new Error('Não foi possível remover o cliente!');
+        }
+    })
+}
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => {
+        if(resposta.ok){
+            resposta.json();
+        }else{
+            throw new Error('Não foi possível detalhar o cliente!');
+        }
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta => {
+        if(resposta.ok){
+            return resposta.json();
+        }else{
+            throw new Error('Não foi possível atualizar o cliente');
+        }
     })
 }
 
 export const clienteService = {
-    listaClientes
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
